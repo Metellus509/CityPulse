45ba87f993173e5e6ab4461d621e2ca5743067de
Metellus509 <metellusjunior56@gmail.com>
2026-05-11 14:41:55 -0400

    first build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.citypulse"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.citypulse"
         minSdk = 26 // Requis par le sujet
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -29,10 +29,27 @@ android {
 
     buildFeatures {
         viewBinding = true // Recommandé pour Empty Views Activity
+        compose = true // Permet d'utiliser les composables de Jetpack Compose
+    }
+
+    composeOptions {
+        // Version du compilateur Kotlin Compose (compatible avec ton Kotlin)
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 }
 
 dependencies {
+
+    // Les bibliothèques indispensables pour Compose Material3
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.01")
+    implementation(composeBom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.8.2")
+
     // --- UI & Lifecycle ---
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
